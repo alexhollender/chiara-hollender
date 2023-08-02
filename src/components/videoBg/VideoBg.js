044cb6ab18de5a1c8c8d8ef51ad7bbc7c03ffbc9
alexhollender <alexhollender@gmail.com>
2023-08-02 17:39:54 -0400

    - vertical video on mobile

--- src/components/videoBg/VideoBg.js
+++ src/components/videoBg/VideoBg.js
@@ -1,9 +1,21 @@
-import styles from './videoBg.module.scss'
+import { headers } from 'next/headers'
 
 export default function VideoBg() {
+
+  const headersList = headers();
+  const userAgent = headersList.get('user-agent');
+  let isMobileView = userAgent && userAgent.match(
+    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+  );
+
   return (
-    <div className="embed-container">
-      <iframe src="https://player.vimeo.com/video/850579349?background=1" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" title="carlotta"></iframe>
-    </div>
+    isMobileView ? 
+      <div className="embed-container">
+        <iframe src="https://player.vimeo.com/video/851069742?background=1" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" title="carlotta"></iframe>
+      </div>
+    :
+      <div className="embed-container">
+        <iframe src="https://player.vimeo.com/video/850579349?background=1" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" title="carlotta"></iframe>
+      </div>
   )
 }
